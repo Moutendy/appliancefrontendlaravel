10892f4cbcf78c2d0eb8e3e459ad4a96d2cd3926
c.moutendy <c.moutendy@isicod.net>
2022-06-14 17:38:17 +0200

    modifier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './views/layout/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: BaseComponent,
+    children: [
+      {
+        path : 'user',
+        loadChildren: () => import('./views/pages/user/user.module').then(m => m.UserModule)
+      },
+      {
+        path : 'admin',
+        loadChildren: () => import('./views/pages/admin/admin.module').then(m => m.AdminModule)
+      },
+      { path: '**', redirectTo: '', pathMatch: 'full' },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
